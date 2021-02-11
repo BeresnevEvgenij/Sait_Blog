82430d277d4bd3d1cd76cec01b7cc578b7ff0996
BeresnevEvgenij <beresnevevgenij24@gmail.com>
2021-02-12 00:27:36 +0300

    JS

--- master.js
+++ master.js
@@ -1,1 +1,31 @@
-// alert("Сайт в разработке");
+const modalBtn = document.querySelectorAll('[data-modal]');
+const modalClose = document.querySelectorAll('.modal_close');
+
+modalBtn.forEach(item => {
+  item.addEventListener('click', event => {
+    console.log('clicked');
+    let $this = event.currentTarget;
+    let modalid = $this.getAttribute('data-modal');
+    let modal = document.getElementById(modalid);
+    let modalContent = modal.querySelector('.modal_content');
+
+    modalContent.addEventListener('click', event => {
+      event.stopPropagation();
+    });
+
+    modal.classList.add('show');
+  });
+});
+
+modalClose.forEach(item => {
+  item.addEventListener('click', event => {
+    let currentModal = event.target.closest('.modal');
+
+    currentModal.classList.remove('show');
+
+  });
+});
+
+function closeModal(currentModal) {
+  currentModal.classList.remove('show');
+}
